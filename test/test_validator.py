4fa21313d89e93540cf08ae910f9bd45e4a6dbb6
Roland Sommer <rol@ndsommer.de>
2015-06-26 12:06:58 +0200

    tests: Increase coverage

--- test/test_validator.py
+++ test/test_validator.py
@@ -1,12 +1,14 @@
 # coding: utf-8
 import os as _os
 import pytest as _pytest
+import mock as _mock
 import cStringIO as _StringIO
 
 import vnu as _vnu
 
 
 BASEDIR = _os.path.abspath(_os.path.dirname(__file__))
+SUPPORTED_OUTPUTS = ["json", "text"]
 
 
 @_pytest.fixture
@@ -14,10 +16,26 @@ def validator():
     return _vnu.HTML5Validator()
 
 
-def test_valid_fragment(validator):
-    status, json = validator.validate_fragment("<div></div>")
+@_pytest.mark.parametrize("url", ["http://test01", "http://test02"])
+def test_service_config(url):
+    service = _vnu.HTML5Validator(url)
+    assert service.validator_url == url
+
+
+@_pytest.mark.parametrize("out", SUPPORTED_OUTPUTS)
+def test_output_modes(validator, out):
+    status, output = validator.validate_fragment("<div></div>", params={"out": out})
     assert status == 200
-    assert all([msg["type"] == "info" for msg in json["messages"]])
+    if out == "json":
+        assert all([msg["type"] == "info" for msg in output["messages"]])
+    else:
+        assert output
+
+
+def test_valid_fragment(validator):
+    status, output = validator.validate_fragment("<div></div>")
+    assert status == 200
+    assert all([msg["type"] == "info" for msg in output["messages"]])
 
 
 def test_invalid_fragment(validator):
@@ -56,3 +74,33 @@ def test_invalid_file_from_fileobj(validator):
     )
     assert status == 200
     assert any([msg["type"] == "error" for msg in json["messages"]])
+
+
+def test_validate_url(validator):
+    status, json = validator.validate_url(validator.validator_url)
+    assert status == 200
+    assert "messages" in json
+
+
+def test_validate_guess_fragment(validator):
+    validator.validate_fragment = _mock.MagicMock()
+    validator.validate("<div></div>")
+    validator.validate_fragment.assert_called_once()
+
+
+def test_validate_guess_document(validator):
+    validator.validate_document = _mock.MagicMock()
+    validator.validate("<!doctype html><div></div>")
+    validator.validate_document.assert_called_once()
+
+
+def test_validate_guess_filename(validator):
+    validator.validate_file = _mock.MagicMock()
+    validator.validate("testdata/test.html")
+    validator.validate_file.assert_called_once()
+
+
+def test_validate_guess_url(validator):
+    validator.validate_url = _mock.MagicMock()
+    validator.validate(validator.validator_url)
+    validator.validate_url.assert_called_once()
